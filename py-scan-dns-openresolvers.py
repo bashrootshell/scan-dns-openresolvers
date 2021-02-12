3243e849b31f7a6f665c1de5c29e3cecb93dd43c
bashrootshell <cicero.sec@gmail.com>
2021-02-12 09:54:13 -0300

    update

--- py-scan-dns-openresolvers.py
+++ py-scan-dns-openresolvers.py
@@ -10,8 +10,7 @@ import socket as com
     Python DNS Open Resolver Scanner
     Originally written in 2013 in Python2. Refactored in 2021 with Python3.
     It still uses Pure Python and the Low-level networking interface.
-    It's faster than nmap dns-recursion script, even for a
-    large CIDR network.
+    It's faster than nmap dns-recursion script, even for a large CIDR network.
 
     PEP8 compliant
     “Readability counts."
@@ -20,10 +19,12 @@ import socket as com
 """
 
 ''' set BSD socket to use a UDP datagram '''
+
 packet = com.socket(com.AF_INET, com.SOCK_DGRAM)
 packet.settimeout(0.150)
 
 ''' DNS type "A" set to l.root-servers.net as hexadecimal value '''
+
 payloadhex = 'ff0001000001000000000000016c0c726f6f742d73657276657273036e6574\
 0000010001'
 
@@ -31,6 +32,7 @@ iplist = []
 
 ''' check if there's an argument, and check if IP address is larger than a /32.
 if not, there's 1 IP only in the list '''
+
 if len(argv) == 1:
     exit("Provide a single IP Address or an IP/CIDR format as the argument.")
 elif ip_network(argv[1]).num_addresses > 1:
@@ -43,10 +45,12 @@ shuffle(iplist)
 for prefix in iplist:
     try:
         ''' send the payloadhex to hosts in the list (port 53 (DNS)) '''
+
         packet.sendto(bytes.fromhex(payloadhex), (str(prefix), int(53)))
         response = packet.recv(128)
         ''' if response contains valid DNS response with the correct
         l.root-servers.net's IP address and type A query, it's open '''
+
         if (b'\xc0\x0c\x00\x01\x00\x01' and b'\x00\x04\xc7\x07') in response:
             nixtime = int(time())
             print(f'{prefix},open,{nixtime}')
